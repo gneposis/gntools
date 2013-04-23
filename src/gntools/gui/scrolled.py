d77d6bb51f231605e4bbff8a781e0fd727bdd5aa
Szieberth Ádám <sziebadam@gmail.com>
2013-04-23 07:14:56 +0000

    commit 004

---
+++ src/gntools/gui/scrolled.py
@@ -0,0 +1,36 @@
+import tkinter
+
+# code from Mark Lutz -- Programming Python (4th edition)
+class List(tkinter.Frame):
+    def __init__(self, options, parent=None):
+        super().__init__(parent)
+        self.make_widgets()
+        self.make_options(options)
+
+    def make_widgets(self, selectmode=tkinter.EXTENDED):
+        sbar = tkinter.Scrollbar(self)
+        self.list = tkinter.Listbox(self,
+                                    relief=tkinter.SUNKEN,
+                                    selectmode=selectmode,
+                                    exportselection=0,
+                                    width=0)
+        sbar.config(command=self.list.yview)
+        self.list.config(yscrollcommand=sbar.set)
+        sbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+        self.list.pack(side=tkinter.LEFT, 
+                       expand=tkinter.YES, 
+                       fill=tkinter.BOTH)
+
+
+    def make_options(self, options):
+        self.list.delete(0, tkinter.END)
+        pos = 0
+        for label in options:
+            self.list.insert(pos, label)
+            pos += 1
+
+       
+
+if __name__ == '__main__':
+    options = (('Lumberjack {}'.format(x)) for x in range(1, 21))
+    List(options).mainloop()
